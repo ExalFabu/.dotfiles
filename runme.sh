2e772393466a5aee675285d27ffb0dbbf021bfcc
Alex <53974096+ExalFabu@users.noreply.github.com>
2022-03-17 18:52:20 +0100

    Refactored folder structure

--- runme.sh
+++ runme.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if ! command -v stow &> /dev/null
 then
@@ -6,7 +6,7 @@ then
     exit
 fi
 
-stow -nvt ~ --ignore=$(cat .stowignore) .
+stow -nvt ~ configs
 echo "---"
 echo -n "Do you wish to proceed? [y/N] "
 # Bunch of code just to ask a single character to confirm the operation. Thx to https://stackoverflow.com/a/27875395/15438584
@@ -14,7 +14,7 @@ old_stty_cfg=$(stty -g)
 stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg # Careful playing with stty
 if echo "$answer" | grep -iq "^y" ;then
     echo ""
-    stow -vt ~ --ignore=$(cat .stowignore) .
+    stow -t ~ configs
 else
     echo "\nAborted"
 fi
